950b327b660b6bbafec6177b62b25536be2beb34
Cayke <caykedaniel@gmail.com>
2022-10-13 17:05:21 -0300

    Complete header styles

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Header } from "./components/Header";
+import "./styles/global.css";
+export function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
